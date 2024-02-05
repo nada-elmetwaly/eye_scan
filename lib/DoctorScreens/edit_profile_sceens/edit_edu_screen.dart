9c7ae4719035dec8f92f53d52d6070bca2a4d431
noda-elmetwaly <elmetwalynada0@gmail.com>
2024-02-05 15:22:53 +0200

    edit education screen

--- lib/DoctorScreens/edit_profile_sceens/edit_edu_screen.dart
+++ lib/DoctorScreens/edit_profile_sceens/edit_edu_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
 
+
+import 'package:flutter/material.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'edit_profile_screen.dart';
 
 class EditEduScreen extends StatefulWidget {
@@ -10,6 +13,7 @@ class EditEduScreen extends StatefulWidget {
 }
 
 class _EditEduScreenState extends State<EditEduScreen> {
+  String dropdownvalue = 'one';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +37,125 @@ class _EditEduScreenState extends State<EditEduScreen> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 90),
+        child: Column(
+          children: [
+            Center(
+              child: CountryCodePicker(
+                initialSelection: 'Eg',
+                showCountryOnly: true,
+                showDropDownButton: true,
+                showOnlyCountryWhenClosed: true,
+                alignLeft: true,
+
+              ),
+            ),
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(top: 9),
+                      child: DropdownButton<String>(
+                        underline: Container(
+                          color: Colors.white,
+                        ),
+                        value: dropdownvalue,
+                        icon: Icon(
+                          Icons.arrow_drop_down,
+                          color: Color(0xff787676),
+                        ),
+                        items: [
+                          DropdownMenuItem<String>(
+                              value: 'one',
+                              child: Text(
+                                'Master',
+                                style: TextStyle(color: Color(0xff787676)),
+                              )),
+                          DropdownMenuItem<String>(
+                              value: 'two',
+                              child: Text(
+                                'Ph.D',
+                                style: TextStyle(color: Color(0xff787676)),
+                              )),
+                        ],
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropdownvalue = value!;
+                          });
+                        },
+                      ),
+                    ),
+
+                    hintText: 'Degree',
+                    hintStyle: TextStyle(
+                        color: Color(0xFF263238),
+                        fontSize: 16,
+                        fontFamily: "myfont",
+                        ),
+                    border: InputBorder.none),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(top: 9),
+                      child: DropdownButton<String>(
+                        underline: Container(
+                          color: Colors.white,
+                        ),
+                        value: dropdownvalue,
+                        icon: Icon(
+                          Icons.arrow_drop_down,
+                          color: Color(0xff787676),
+                        ),
+                        items: [
+                          DropdownMenuItem<String>(
+                              value: 'one',
+                              child: Text(
+                                'Harvard',
+                                style: TextStyle(color: Color(0xff787676)),
+                              )),
+                          DropdownMenuItem<String>(
+                              value: 'two',
+                              child: Text(
+                                'Jhon',
+                                style: TextStyle(color: Color(0xff787676)),
+                              )),
+
+
+                        ],
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropdownvalue = value!;
+                          });
+                        },
+                      ),
+                    ),
+
+                    hintText: 'University',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF263238),
+                      fontSize: 16,
+                      fontFamily: "myfont",
+                    ),
+                    border: InputBorder.none),
+              ),
+            ),
+            SizedBox(height: 30,),
+
+          ],
+        ),
+      ),
+
+
     );
   }
 }
