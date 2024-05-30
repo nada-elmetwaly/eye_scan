169015b9a615e162a2d2935fcf47807845c45e75
nada-2003 <112822256+nada-2003@users.noreply.github.com>
2024-05-30 22:45:49 +0300

    Delete eye_scan-main directory

--- eye_scan-main/android/app/src/main/kotlin/com/example/eye_scan/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eye_scan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- eye_scan-main/lib/components/maincolor.dart
+++
@@ -1,4 +0,0 @@
-import 'dart:ui';
-
-const Color?maincolorblue= Color(0xFF73B8EB);
-const Color?maincolorgrey= Color(0xff979797);
